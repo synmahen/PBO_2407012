7d24805a7d21f0d0ee29c1a8173655f6f76012eb
synmahen <synmahendra@gmail.com>
2026-03-09 20:02:30 +0700

    tugas

--- latihan_2407012/src/latihan_pbo/latihan3_2407012/Main.java
+++ latihan_2407012/src/latihan_pbo/latihan3_2407012/Main.java
@@ -1,28 +1,50 @@
 package latihan_pbo.latihan3_2407012;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main (String[] arg) {
-        SegiTiga sTiga = new SegiTiga();
-        PersegiPanjang pp = new PersegiPanjang();
-        JajarGenjang jGenjang = new JajarGenjang();
+    public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
 
-        pp.setPanjang(7);
-        pp.setLebar(19);
-        System.out.println("Lebar Persegi panjang " + pp.LebarPersegiPanjang());
+        System.out.println("Pilih Bangun Datar");
+        System.out.println("1. Persegi Panjang");
+        System.out.println("2. Jajar Genjang");
+        System.out.println("3. Segitiga");
+        System.out.print("Masukkan pilihan: ");
+        int pilihan = input.nextInt();
 
-        jGenjang.setAlas(17);
-        jGenjang.setTinggi(7);
-        System.out.println("Luas Jajar Genjang adalah : " + jGenjang.LuasJajarGenjang());
+        if (pilihan == 1) {
+            System.out.print("Masukkan panjang: ");
+            double panjang = input.nextDouble();
+            System.out.print("Masukkan lebar: ");
+            double lebar = input.nextDouble();
+            System.out.println("Luas Persegi Panjang = " + (panjang * lebar));
+        }
 
+        else if (pilihan == 2) {
+            System.out.print("Masukkan alas: ");
+            double alas = input.nextDouble();
+            System.out.print("Masukkan tinggi: ");
+            double tinggi = input.nextDouble();
+            System.out.println("Luas Jajar Genjang = " + (alas * tinggi));
+        }
 
-        sTiga.setAlas(2);
-        sTiga.setTinggi(3);
-        System.out.println("Luas segitiga adalah : " + sTiga.LuasSegiTiga());
+        else if (pilihan == 3) {
+            System.out.print("Masukkan alas: ");
+            double alas = input.nextDouble();
+            System.out.print("Masukkan tinggi: ");
+            double tinggi = input.nextDouble();
+            System.out.println("Luas Segitiga = " + (0.5 * alas * tinggi));
+        }
 
+        else {
+            System.out.println("Pilihan tidak tersedia");
+        }
+    }
+}
     
 //dibikin selection switch case atau if else pada bagian input variablenya dari manual nanti dibikin ada pilihan nonor 1 persegi nomor 2 perseigi panjang, nanti masukan pilihan persegi 
 // nanti pas keluar di itu keluar lagi ada sisi ada panjang nya gitu jadi nginput di terminalnya
 
-    }
-}
+  
