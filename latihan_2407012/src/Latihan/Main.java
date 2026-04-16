34c61d698bf9b6da3950dc48e18b39eee2a50449
synmahen <synmahendra@gmail.com>
2026-04-16 09:18:53 +0700

    Tugas 3

--- latihan_2407012/src/Latihan/Main.java
+++ latihan_2407012/src/Latihan/Main.java
@@ -1,5 +1,8 @@
 package Latihan;
 
 public class Main {
-    
+     public static void main(String[] args) {
+        MenuItem item1 = new MenuItem("Mie Ayam", 12000, 3);
+        item1.tampilkan();
+    }
 }
--- latihan_2407012/src/Latihan/Menu.java
+++ latihan_2407012/src/Latihan/Menu.java
@@ -1,5 +1,27 @@
 package Latihan;
 
 public class Menu {
-    
+    String nama;
+    int harga;
+    int jumlah;
+
+    // Constructor
+    public Menu(String nama, int harga, int jumlah) {
+        this.nama = nama;
+        this.harga = harga;
+        this.jumlah = jumlah;
+    }
+
+    // Method hitung total
+    public int hitungTotal() {
+        return harga * jumlah;
+    }
+
+    // Method tampilkan
+    public void tampilkan() {
+        System.out.println("Nama Menu  : " + nama);
+        System.out.println("Harga      : " + harga);
+        System.out.println("Jumlah     : " + jumlah);
+        System.out.println("Total      : " + hitungTotal());
+    }
 }
